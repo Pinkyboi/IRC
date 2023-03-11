351aabb51b8ff6e3350cdc6bf3d4820ccd516fab
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2023-03-11 17:52:17 +0100

    Make nicks global to the server

--- src/Channel/inc/Channel.hpp
+++ src/Channel/inc/Channel.hpp
@@ -13,20 +13,16 @@ class Channel
         void                    remove_client(int client_id);
     public:
         bool                    is_client(int client_id);
-        bool                    is_nick_used(std::string& nick);
     public:
-        int                     get_client_id(std::string& nick);
         Client&                 get_client(int client_id);
         std::string             get_name() const;
         std::string             get_topic() const;
         int                     get_clients_count() const;
     public:
         void                    set_topic(std::string topic);
-        void                    update_nick(int client_id, std::string nick);
     private:
         const std::string           _name;
         std::string                 _topic;
-        std::map<std::string, int>  _nicks;
         std::map<int, Client&>      _clients;
 };
 
