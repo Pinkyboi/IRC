239070ed2fa3f0e8fe73902c1b36893e427f04d9
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-03-11 22:02:15 +0100

    update parser

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -3,34 +3,18 @@
 Parser::Parser() {}
 Parser::~Parser() {}
 
-static std::string strip(std::string str)
-{
-    size_t  f, l;
-    f = str.find_first_not_of(" \0");
-    l = str.find_last_not_of(" \0");
-    return str.substr(f, l-f+1);
-}
-
-std::vector<std::string>    split_command(std::string message, std::string sep, size_t times)
+std::vector<std::string>    split_command(std::string message, std::string sep)
 {
     std::vector<std::string>    tokens;
     std::string                 token;
     size_t                      i, j;
     size_t                      f, l;
 
-    message = strip(message);
     j = 0;
-    for (i = 0; j != std::string::npos && times; i = j + 1)
+    for (i = 0; j != std::string::npos; i = j + 1)
     {
         j = message.find_first_of(sep, i);
         token = message.substr(i, j - i);
-        if (token.size())
-            tokens.push_back(token);
-        times = times == -1 ? times : --times;
-    }
-    if (times == 0)
-    {
-        token = message.substr(i, message.size() - i);
         if (token.size())
             tokens.push_back(token);
     }
@@ -59,14 +43,17 @@ void    Parser::parse(std::string message)
     _arguments.clear();
     _message.clear();
 
-    tokens = split_command(message, ":", 1);
-    std::cout << tokens.size() << std::endl;
-    if (tokens.size())
+    // Find the message part.
+    size_t pos = message.find_first_of(":");
+    if (pos != std::string::npos)
     {
-        _message = std::string(tokens[tokens.size() - 1]);
-        tokens = split_command(tokens[0], " ", -1);
-        _command = std::string(tokens[0]);
-        for (size_t i = 1; i < tokens.size(); i++)
-            _arguments.push_back(std::string(tokens[i]));
+        _message = message.substr(pos+1, message.size() - pos);
+        message = message.substr(0, pos);
     }
+
+    // Find the command and parameters part.
+    tokens = split_command(message, " ");
+    _command = std::string(tokens[0]);
+    for (size_t i = 1; i < tokens.size(); i++)
+        _arguments.push_back(std::string(tokens[i]));
 }
